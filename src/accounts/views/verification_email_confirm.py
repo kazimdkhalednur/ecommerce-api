0864ab34f032ea35c5fc428c3384f32fdfa27c69
Kazi Khaled Nur <kazikhalednur@gmail.com>
2025-06-11 21:29:34 +0600

    update verification mail view

--- src/accounts/views/verification_email_confirm.py
+++ src/accounts/views/verification_email_confirm.py
@@ -42,6 +42,12 @@ class VerificationEmailConfirmView(APIView):
                 status_code=status.HTTP_400_BAD_REQUEST, message="Invalid link"
             )
 
+        if user.is_active:
+            return Response(
+                status_code=status.HTTP_200_OK,
+                message="You are already verified your mail address",
+            )
+
         if token_generator.check_token(user, token):
             user.is_active = True
             user.save(update_fields=["is_active"])
